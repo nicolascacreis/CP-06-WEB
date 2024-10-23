24b1ce59b904614cfaded02499e51967b5ea8a17
nicolascacreis <162995749+nicolascacreis@users.noreply.github.com>
2024-10-23 10:11:36 -0300

    Add files via upload

---
+++ src/pages/categorias.js
@@ -0,0 +1,13 @@
+import Header from '../component/Header'
+import AppContainer from '../component/AppContainer'
+
+const Categorias = () => {
+  return (
+    <AppContainer>
+      <Header />
+      <h1>CATEGORIAS</h1>
+    </AppContainer>
+  );
+}
+
+export default Categorias
